3918e2505db0b4eccf920c77721c44bbaf6514fd
Kenan Casey <kenancasey@gmail.com>
2021-02-24 19:50:02 -0600

    Initial commit

---
+++ Sorting Algorithms/ISortable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4.SortingAlgorithms
+{
+    public interface ISortable<T> where T : IComparable<T>
+    {
+        void Sort(ref List<T> items);
+    }
+
+    public interface ISortableInt
+    {
+        int[] Sort(int[] array);
+    }
+}
